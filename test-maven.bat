df94be8b7b0ff7e335feed1da50ace2165de0feb
cliff.meyers <cliff.meyers@gmail.com>
2011-07-16 16:26:18 -0400

    rename artifact / module for Maven plugin

--- test-maven.bat
+++ test-maven.bat
@@ -2,7 +2,7 @@ set MAVEN_OPTS=-Xmx512m -XX:MaxPermSize=128m
 
 cmd /C %M2_HOME%\bin\mvn.bat install -f java2as-core/pom.xml -Dmaven.test.skip=true
 
-cmd /C %M2_HOME%\bin\mvn.bat install -f java2as-maven/pom.xml -Dmaven.test.skip=true
+cmd /C %M2_HOME%\bin\mvn.bat install -f java2as-maven-plugin/pom.xml -Dmaven.test.skip=true
 
 set MAVEN_OPTS=-Xmx512m -XX:MaxPermSize=128m -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y
 
