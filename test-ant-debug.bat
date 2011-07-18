2ce549b4dcf769c557654dea774ac2af670795a9
cliff.meyers <cliff.meyers@gmail.com>
2011-07-18 00:50:31 -0400

    update test scripts for regular / debug mode

--- test-ant-debug.bat
+++ test-ant-debug.bat
@@ -4,6 +4,6 @@ cmd /C %M2_HOME%\bin\mvn.bat install -f java2as-core/pom.xml -Dmaven.test.skip=t
 
 cmd /C %M2_HOME%\bin\mvn.bat install -f java2as-ant-task/pom.xml -Dmaven.test.skip=true
 
-set ANT_OPTS=-Xmx512m -XX:MaxPermSize=128m
+set ANT_OPTS=-Xmx512m -XX:MaxPermSize=128m -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y
 
 cmd /C %ANT_HOME%\bin\ant.bat test -f java2as-ant-test/build.xml
