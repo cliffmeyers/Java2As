c4f09ed7eef560040076e93c2575487dce5c8c7f
cliff.meyers <cliff.meyers@gmail.com>
2011-07-23 21:52:25 -0400

    update list of all tests

--- java2as-core/src/test/java/net/histos/java2as/AllTests.java
+++ java2as-core/src/test/java/net/histos/java2as/AllTests.java
@@ -1,8 +1,13 @@
 package net.histos.java2as;
 
 import net.histos.java2as.as3.DefaultAs3TypeMapperTests;
+import net.histos.java2as.as3.service.ServiceDelegateProducerTests;
 import net.histos.java2as.as3.transfer.As3PropertyTests;
+import net.histos.java2as.as3.transfer.TransferObjectMapperTests;
 import net.histos.java2as.as3.transfer.TransferObjectProducerTests;
+import net.histos.java2as.as3.transfer.extensions.AmfIgnoreAs3PropertyMapperTests;
+import net.histos.java2as.as3.transfer.extensions.JpaIdentifierAs3PropertyMapperTests;
+import net.histos.java2as.core.PackageMapperTests;
 import net.histos.java2as.core.meta.JavaMethodTests;
 import net.histos.java2as.core.meta.JavaTransferObjectTests;
 import org.junit.runner.RunWith;
@@ -10,9 +15,14 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-		As3PropertyTests.class,
-		TransferObjectProducerTests.class,
 		DefaultAs3TypeMapperTests.class,
+		ServiceDelegateProducerTests.class,
+		As3PropertyTests.class,
+		TransferObjectMapperTests.class,
+		TransferObjectProducerTests.class,
+		AmfIgnoreAs3PropertyMapperTests.class,
+		JpaIdentifierAs3PropertyMapperTests.class,
+		PackageMapperTests.class,
 		JavaMethodTests.class,
 		JavaTransferObjectTests.class
 })
