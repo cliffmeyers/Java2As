9fb8012add481dcfa738c0b263ef95f0926bd114
cliff.meyers <cliff.meyers@gmail.com>
2011-08-04 14:08:50 -0400

    use clean builds when testing Ant tasks

--- test-ant-debug.bat
+++ test-ant-debug.bat
@@ -1,8 +1,8 @@
 set MAVEN_OPTS=-Xmx512m -XX:MaxPermSize=128m
 
-cmd /C %M2_HOME%\bin\mvn.bat install -f java2as-core/pom.xml -Dmaven.test.skip=true
+cmd /C %M2_HOME%\bin\mvn.bat clean install -f java2as-core/pom.xml -Dmaven.test.skip=true
 
-cmd /C %M2_HOME%\bin\mvn.bat install -f java2as-ant-task/pom.xml -Dmaven.test.skip=true
+cmd /C %M2_HOME%\bin\mvn.bat clean install -f java2as-ant-task/pom.xml -Dmaven.test.skip=true
 
 set ANT_OPTS=-Xmx512m -XX:MaxPermSize=128m -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y
 
